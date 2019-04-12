a4a22b7c542f9d5241f2b2ca98debc564a41bb8a
Brendan Moroso <brendan.moroso@gmail.com>
2019-04-12 11:20:20 -0700

    Add logic for pizza price and new  order function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 function Pizza(pizzaToppings, pizzaSize, pizzaPrice) {
-  this.pizzaToppings = pizzaToppings;
   this.pizzaSize = pizzaSize;
-  this.pizzaPrice = pizzaPrice;
+  this.pizzaToppings = pizzaToppings;
+  this.pizzaPrice = 10;
 }
 
-// Pizza.prototype.order = function() {
-//   this.pizzaPrice = 10;
-//   if (this.pizzaToppings === 1) {
-//     this.pizzaPrice += 12;
-//   }
-// }
+Pizza.prototype.orderPrice = function() {
+  this.pizzaPrice += this.pizzaToppings;
+  if (this.pizzaSize === 1){
+    this.pizzaPrice += 0;
+  } else if (this.pizzaSize === 2) {
+    this.pizzaPrice += 4;
+  } else if (this.pizzaSize === 3) {
+    this.pizzaPrice += 6;
+  }
+}
 
-
+var order = new Pizza();
 //User Interface Logic
-var newOrder = new Pizza();
+function newOrder(inputtedPizzaSize, inputtedPizzaToppings) {
+  inputtedPizzaSize = $("input#pizza-size").val();
+  inputtedPizzaToppings = $("input#pizza-toppings").val();
+  order.pizzaSize = inputtedPizzaSize;
+  order.pizzaToppings = inputtedPizzaToppings;
+}
 
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
     event.preventDefault();
-    var inputtedPizzaToppings = $("input#pizza-toppings").val();
-    var inputtedPizzaSize = $("input#pizza-size").val();
-    $("input#pizza-toppings").val("");
-    $("input#pizza-size").val("");
+    newOrder();
    });
  });

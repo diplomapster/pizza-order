999991c4125e4b40b3ce26f1d9bb51b08f8b254e
Brendan Moroso <brendan.moroso@gmail.com>
2019-04-12 09:15:02 -0700

    Add pizza constructor.

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Pizza Order_
+
+##### _A tool to determine how much your pizza costs, April 2019_
+
+##### By **Brendan Moroso**
+
+## Description
+
+This project contains a survey by Brendan Moroso, a student in the March 2019 Cohort of Epicodus in Seattle, to be used to
+
+## Specs
+1.
+
+## Setup/Installation Requirements
+
+* Visit view the contents of this webpage. The site itself is not currently live.
+
+## Support and contact details
+
+If there are any issues, please contact the creator on github at [Diplomapster](https://github.com/diplomapster)
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* jQuery
+
+### License
+
+*MIT 2019*
+
+Copyright (c) 2019 **Brendan Moroso**
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
-pizza constructor
-  this.pizzaToppings: toppings
-  this.pizzaSize:
-  this.pizzaPrice:
+function Pizza(pizzaToppings, pizzaSize, pizzaPrice) {
+  this.pizzaToppings = pizzaToppings;
+  this.pizzaSize = pizzaSize;
+  this.pizzaPrice = pizzaPrice;
+}
+
+Pizza.prototype.order = (function) {
+  this.pizzaPrice = 10;
+  if 
+}
+
+
+//User Interface Logic
+var newOrder = new Order(pizzaToppings, pizzaSize);
+
+$(document).ready(function() {
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+    var inputtedPizzaToppings = $("input#pizza-toppings").val();
+    var inputtedPizzaSize = $("input#pizza-size").val();
+    $("input#pizza-toppings").val("");
+    $("input#pizza-size").val("");
+   });
+ });
